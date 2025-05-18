85381a07d0b8b38889bca4cfd0509de7abee991c
Bisrat Gizaw <bisrojc60@gmail.com>
2025-05-18 23:24:03 +0300

    updates

--- listings/urls.py
+++ listings/urls.py
@@ -1,1 +1,6 @@
-<listings urls content here>
+from django.urls import path
+from .views import WelcomeView
+
+urlpatterns = [
+    path('', WelcomeView.as_view(), name='welcome'),
+]
--- listings/views.py
+++ listings/views.py
@@ -1,1 +1,6 @@
-<views content here>
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class WelcomeView(APIView):
+    def get(self, request):
+        return Response({'message': 'Welcome to the ALX Travel App API!'})
