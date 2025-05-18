7f72541d48c0f2e1e88c6219cfd90d16b93a8eee
Bisrat Gizaw <bisrojc60@gmail.com>
2025-05-18 23:35:29 +0300

    Delete listings directory

--- listings/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-class WelcomeView(APIView):
-    def get(self, request):
-        return Response({'message': 'Welcome to the ALX Travel App API!'})
